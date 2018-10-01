140be00a6f29811ba778e9cb984959f8fc846b2d
maizz <mizzouzi@efficom-lille.com>
2018-10-01 20:43:33 +0200

    [MI][1]create project structure

--- src/WorkshopBundle/Controller/EpsiController.php
+++ src/WorkshopBundle/Controller/EpsiController.php
@@ -5,13 +5,13 @@ namespace WorkshopBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+class EpsiController extends Controller
 {
     /**
      * @Route("/")
      */
     public function homeAction()
     {
-        return $this->render('WorkshopBundle:Default:homepage.html.twig');
+        return $this->render('WorkshopBundle:Epsi:subhomepage.html.twig');
     }
 }
