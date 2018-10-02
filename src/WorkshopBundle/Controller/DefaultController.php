70fdc360848ebe1a678e32ebb05835812a25e884
maizz <mizzouzi@efficom-lille.com>
2018-10-02 10:25:22 +0200

    [MI][2]fix bug

--- src/WorkshopBundle/Controller/DefaultController.php
+++ src/WorkshopBundle/Controller/DefaultController.php
@@ -6,9 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
     public function homeAction()
     {
         return $this->render('WorkshopBundle:Default:homepage.html.twig');
--- src/WorkshopBundle/Controller/WisController.php
+++ src/WorkshopBundle/Controller/WisController.php
@@ -6,9 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class WisController extends Controller
 {
-    /**
-     * @Route("/")
-     */
     public function homeAction()
     {
         return $this->render('WorkshopBundle:Wis:subhomepage.html.twig');
