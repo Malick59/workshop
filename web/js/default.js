1e8258cc1f327dedc04ec9deeb9e64641fb76df9
jeremygil <jeremy.gilger@hotmail.fr>
2018-10-03 10:21:06 +0200

    modif css

--- web/js/default.js
+++ web/js/default.js
@@ -10,8 +10,37 @@ $("#ifag").hover(function(){
     }, function(){
     $(".baseline").css("color", "white");
 });
-$("#wiz").hover(function(){
+$("#wis").hover(function(){
     $(".baseline").css("color", "#FDEA18");
     }, function(){
     $(".baseline").css("color", "white");
 });
+
+/*  */
+
+// $("#epsi").hover(function(){
+	// var $this = $(this).text();
+    // $(".textFooter").text("EPSI L'École d'ingénierie informatique");
+    // }, function(){
+	// $($this).text(this);
+// });
+
+$(function(){
+	var origine = "Découvrir notre campus à l'aide de l'une des 3 écoles fondatrices";
+	
+  $('#epsi').hover(function(){
+      $(".textFooter").text("EPSI L'École d'ingénierie informatique");
+  }, function(){
+      $(".textFooter").text(origine);
+  });
+    $('#wis').hover(function(){
+      $(".textFooter").text("WIS L'École Web et digital business");
+  }, function(){
+      $(".textFooter").text(origine);
+  });
+    $('#ifag').hover(function(){
+      $(".textFooter").text("IFAG L'École de management pour entreprendre & innover");
+  }, function(){
+      $(".textFooter").text(origine);
+  });
+})
