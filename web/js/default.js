c302b435a5ec3a809132cd9f912fb248b4e8cfa3
jeremygil <jeremy.gilger@hotmail.fr>
2018-10-03 09:19:11 +0200

    modif color portail

---
+++ web/js/default.js
@@ -0,0 +1,17 @@
+/* Color text portail */
+
+$("#epsi").hover(function(){
+    $(".baseline").css("color", "#271549");
+    }, function(){
+    $(".baseline").css("color", "white");
+});
+$("#ifag").hover(function(){
+    $(".baseline").css("color", "#D50100");
+    }, function(){
+    $(".baseline").css("color", "white");
+});
+$("#wiz").hover(function(){
+    $(".baseline").css("color", "#FDEA18");
+    }, function(){
+    $(".baseline").css("color", "white");
+});
