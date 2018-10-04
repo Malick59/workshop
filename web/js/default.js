a522017548c51402a12896eb838d8ead4b9c15b9
tricartc <clem.tric@hotmail.fr>
2018-10-04 12:04:05 +0200

    rebase 2 conflicts

--- web/js/default.js
+++ web/js/default.js
@@ -1,49 +1,33 @@
 /* Color text portail */
-
-$("#epsi").hover(function(){
-    $(".baseline").css("color", "#271549");
-    }, function(){
-    $(".baseline").css("color", "white");
-});
-$("#ifag").hover(function(){
-    $(".baseline").css("color", "#D50100");
-    }, function(){
-    $(".baseline").css("color", "white");
-});
-$("#wis").hover(function(){
-    $(".baseline").css("color", "#FDEA18");
-    }, function(){
-    $(".baseline").css("color", "white");
-});
-
-/*  */
-
-// $("#epsi").hover(function(){
-	// var $this = $(this).text();
-    // $(".textFooter").text("EPSI L'École d'ingénierie informatique");
-    // }, function(){
-	// $($this).text(this);
-// });
-
 $(function(){
 	var origine = "Découvrir notre campus à l'aide de l'une des 3 écoles fondatrices";
 	
   $('#epsi').hover(function(){
-      $(".textFooter").text("EPSI L'École d'ingénierie informatique");
+      $(".textFooter").text("EPSI L'École d'ingénierie informatique").css("color","#ffffff");
+      $(".wave-svg").css("fill","#4f4091");
+
   }, function(){
-      $(".textFooter").text(origine);
+      $(".textFooter").text(origine).css("color","#000000");
+      $(".wave-svg").css("fill","#ffffff");
+
   });
     $('#wis').hover(function(){
       $(".textFooter").text("WIS L'École Web et digital business");
+      $(".wave-svg").css("fill","#fbdc00");
+
   }, function(){
       $(".textFooter").text(origine);
+      $(".wave-svg").css("fill","#ffffff");
   });
     $('#ifag').hover(function(){
-      $(".textFooter").text("IFAG L'École de management pour entreprendre & innover");
+      $(".textFooter").text("IFAG L'École de management pour entreprendre & innover").css("color","#ffffff");
+      $(".wave-svg").css("fill","#e30613");
+
   }, function(){
-      $(".textFooter").text(origine);
+      $(".textFooter").text(origine).css("color","#000000");
+      $(".wave-svg").css("fill","#ffffff");
   });
-})
+});
 
 /* Ne pas glisser les images */
 $(function () {
